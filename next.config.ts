5b323da4ef8b7ce7037f1cd198d316009ec13fa9
developer001 <scsdeveloper001@gmail.com>
2025-03-29 21:48:58 +0530

    update structure by removing files

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: `/${process.env.ADMIN_SECURE_ROUTE_PATH}/:path*`,
-        destination: "/admin/:path*",
-      },
-    ];
-  }
+ 
 };
 
 export default nextConfig;
